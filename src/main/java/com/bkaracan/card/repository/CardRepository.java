066aab8cf8a386b6c43bdd59e36cd13bfdb5c717
bkaracan <burakkkaracan94@gmail.com>
2023-12-07 16:03:22 +0300

    entity and repository layer created

---
+++ src/main/java/com/bkaracan/card/repository/CardRepository.java
@@ -0,0 +1,14 @@
+package com.bkaracan.card.repository;
+
+import com.bkaracan.card.entity.Card;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Optional<Card> findByMobileNumber(String mobileNumber);
+
+    Optional<Card> findByCardNumber(String cardNumber);
+}
